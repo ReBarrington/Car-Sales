9ce26c23f17c44ac08f6cd41d807d044edb0a5b9
Reagan Barrington <reagan@barringtonfamily.net>
2020-03-17 16:22:40 -0400

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
+
+const App = props => {
+console.log(props, " props to App")
+
+  const removeFeature = item => {
+    // dispatch an action here to remove an item
+    return { type: 'REMOVE_FEATURE'} ;
+  };
+
+  const buyItem = item => {
+    // dipsatch an action here to add an item
+    return { type: 'BUY_ITEM', payload: "buy this item" } ;
+  };
+
+  return (
+    <div className="boxes">
+      <div className="box">
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
+      </div>
+      <div className="box">
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
+      </div>
+    </div>
+  );
+};
+
+
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
     car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
+      price: state.car.price,
+      name: state.car.name,
       image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
+        state.car.image,
+      features: [state.car.features]
     },
     additionalFeatures: [
       { id: 1, name: 'V-6 engine', price: 1500 },
@@ -21,28 +51,10 @@ const App = () => {
       { id: 3, name: 'Premium sound system', price: 500 },
       { id: 4, name: 'Rear spoiler', price: 250 }
     ]
-  };
+  }
+}
 
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
-
-  return (
-    <div className="boxes">
-      <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
-      </div>
-      <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
-      </div>
-    </div>
-  );
-};
-
-export default App;
+export default connect(
+  mapStateToProps,
+  // { CASE_1 } // same as { toggleEditing: toggleEditing }
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers/reducer';
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+  console.log(store.getState, ' is store.getState in createStore')
+
+
+  const rootElement = document.getElementById('root');
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        rootElement
+        );
+
+const mapStateToProps = state => {
+    return {
+        
+    }
+    }
