077c73f0bcb1278817dd033cb66903c731c18367
Reagan Barrington <reagan@barringtonfamily.net>
2020-03-18 11:38:01 -0400

    change so each added feature has remove button

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,25 +1,21 @@
 import React from 'react';
 
 const AddedFeature = props => {
-
   console.log(props, ' is props for AddedFeature')
-
   return (
-    <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button 
-        className="button"
-        onClick={() => props.removeFeature(props.feature.id)}
-      >
-        X
-      </button>
-      {props.feature.map(fture => 
-        <ul className="addedfeatures-list">
-          <li>{fture}</li>
-        </ul>
-      )}
-    </li>
-  );
-};
+    props.feature.map(fture => {
+      return (
+      <li>
+        <button 
+          className="button"
+          onClick={() => props.removeFeature(fture.id)}
+        >
+          X
+        </button>
+        {fture}
+      </li>
+      )
+    }
+  ))}
 
 export default AddedFeature;
